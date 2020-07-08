e463027f0e07c2886a3bf57f9ff28f62e1598273
Gustavo Dupin <55361396+iamgriffon@users.noreply.github.com>
2020-07-07 22:07:31 -0300

    A few updates and changes

--- src/components/use-state-example/use-state-example.component.jsx
+++ src/components/use-state-example/use-state-example.component.jsx
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../card/card.component';
 
 const UseStateExample = () => {
-  const [name, setName] = useState('Yihua');
-  const [address, setAddress] = useState('Amsterdam');
+  const [user, setUser] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('Bret');
+
+  useEffect(() => {
+    console.log('Hiop');
+    const fetchFunc = async() => {
+     const response = await fetch(`https://jsonplaceholder.typicode.com/users?username=${searchQuery}`);
+     const resJson = await response.json();
+     setUser(resJson[0]);
+    };
+    fetchFunc();
+  }, [searchQuery]);
 
   return (
     <Card>
-      <h1> {name} </h1>
-      <h1> {address} </h1>
-      <button onClick={() => setName('Andrei')}>Set Name to Andrei</button>
-      <button onClick={() => setAddress('Canada')}>Set Address</button>
+      <input 
+      type="search"
+      value={searchQuery}
+      onChange={event => setSearchQuery(event.target.value)}
+      />
+    {user? ( 
+      <div>
+        <h1> {user.name} </h1>
+        <h1> {user.username} </h1>
+        <h1> {user.email} </h1>
+      </div>
+      ) : (
+        <p>User not found</p>
+      )}
+      {/* <button onClick={() => setName('Andrei')}>Set Name to Andrei</button>
+      <button onClick={() => setAddress('Canada')}>Set Address</button> */}
     </Card>
   );
 };
